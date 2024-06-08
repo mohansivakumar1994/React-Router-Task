d27048034b0a3eba9728edc4f0520f0cba0eb015
MOHAN SIVAKUMAR <139428429+mohansivakumar1994@users.noreply.github.com>
2024-06-09 02:33:16 +0530

    Update All.jsx

--- src/Components/All.jsx
+++ src/Components/All.jsx
@@ -6,7 +6,7 @@ export default function All(){
              {/* Card for Fullstack Development with MORN Stack */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/MORN_Image.jpg" className="card-img-top" alt="" />
+                     <img src="https://www.guvi.in/build/images/women1.555f1fd9c475b67115ac2a2d120a84e8.webp" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Fullstack Development: MORN Stack</h5>
                          <p className="card-text">Learn how to connect ReactJS with NodeJS, Express & MongoDB. Build an entire project from scratch. Refresh the basics about ReactJS, NodeJS, Express and MongoDB</p>
@@ -18,7 +18,7 @@ export default function All(){
              {/* Card for Fullstack Development with MEAN Stack */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/MEAN_Image.png" className="card-img-top" alt="" />
+                     <img src="https://tse4.mm.bing.net/th?id=OIP.ylvI5Jem741I2sN2WySPZAAAAA&pid=Api&P=0&h=180" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Fullstack Development: MEAN Stack</h5>
                          <p className="card-text">Build real Angular + NodeJS applications. Understand how Angular works and how it interacts with Backends. Connect any Angular Frontend with a NodeJS Backend</p>
@@ -30,7 +30,7 @@ export default function All(){
              {/* Card for Cyber Security Course 1 */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/cybersecurity-1.jpg" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awr1RVpFxWRm0SAhdTK9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzY2MTE5NTk0YzE4MmE1MjU4NGJlMWVjOTUwMmM5NmRkBGdwb3MDMTEEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dcyber%2Bsecurity%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D11&w=474&h=316&imgurl=icaagencyalliance.com%2Fwp-content%2Fuploads%2F2018%2F11%2FCyber-Security-min.jpg&rurl=https%3A%2F%2Ficaagencyalliance.com%2Fcyber-security%2F&size=1319.1KB&p=cyber+security+images&oid=66119594c182a52584be1ec9502c96dd&fr2=sa-gp-search&fr=mcafee&tt=Cyber+Security+%E2%80%93+ICA+Agency+Alliance%2C+Inc.&b=0&ni=160&no=11&ts=&tab=organic&sigr=0ve1wNKStQwd&sigb=zAjXwQ_3r1JM&sigi=_wGq374Dd2EI&sigt=MLyzkbdtK_44&.crumb=Rex9syo7NUw&fr=mcafee&fr2=sa-gp-search&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">The Complete Cyber Security Course : Hackers Exposed</h5>
                          <p className="card-text">An advanced practical skillSet in defeating all online threats - advanced hackers, trackers, malware and all Internet nastiness & government mass surveillance.</p>
@@ -42,7 +42,7 @@ export default function All(){
              {/* Card for Cyber Security Course 2 */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/cybersecurity-2.jpg" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awr1RVpFxWRm0SAhfzK9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2RmNzc0OTkyOGYyMTA1ZTcxZDRjYmQwODZlMmVhZjljBGdwb3MDMjEEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dcyber%2Bsecurity%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D21&w=7680&h=3385&imgurl=www.cm-alliance.com%2Fhubfs%2F60776739_l%2520%281%29.jpg%23keepProtocol&rurl=https%3A%2F%2Fwww.empoweryouth.com%2Fskill-up%2Fdetail%2Fwhat-is-cybersecurity-everything-you-need-to-know-1-110488&size=854.2KB&p=cyber+security+images&oid=df7749928f2105e71d4cbd086e2eaf9c&fr2=sa-gp-search&fr=mcafee&tt=Empower+Youth&b=0&ni=160&no=21&ts=&tab=organic&sigr=sKYe66k3bl0O&sigb=5e2_oR0qio8q&sigi=tLoYUxJPTt5s&sigt=xNkFY7H9OD6K&.crumb=Rex9syo7NUw&fr=mcafee&fr2=sa-gp-search&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">2024 Beginners Guide To Cyber Security</h5>
                          <p className="card-text">Cybersecurity Basics: Learn the essential concepts of cybersecurity and the CIA (Confidentiality, Integrity, and Availability) rating.</p>
@@ -54,7 +54,7 @@ export default function All(){
              {/* Card for Data Science Course */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/DATA_SCIENCE_Img.jpg" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awr1RVq7xWRmF6QgzFi9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzJjMWE0ZDRiMDQyNzZlYTQ3NDI0ZWNkN2NjZWY4ZjUzBGdwb3MDMgRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Ddata%2Bscience%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dsa-gp-search%26tab%3Dorganic%26ri%3D2&w=1920&h=1080&imgurl=wallpaperaccess.com%2Ffull%2F1704510.jpg&rurl=https%3A%2F%2Fwallpaperaccess.com%2Fdata-science&size=134.7KB&p=data+science+images&oid=2c1a4d4b04276ea47424ecd7ccef8f53&fr2=sa-gp-search&fr=mcafee&tt=Data+Science+Wallpapers+-+Top+Free+Data+Science+Backgrounds+...&b=0&ni=160&no=2&ts=&tab=organic&sigr=ljQB8p.pTEDJ&sigb=yyYrQ.aaNvty&sigi=VRUBpC2FuV.V&sigt=Pqp0Trmb_HnD&.crumb=Rex9syo7NUw&fr=mcafee&fr2=sa-gp-search&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Data Science A-Z: Hands-On Exercise</h5>
                          <p className="card-text">Successfully perform all steps in a complex Data Science project. Create Basic Tableau visualization's. Perform Data Mining in Tableau.</p>
@@ -66,7 +66,7 @@ export default function All(){
              {/* Card for Statistics Course */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100 mb-3">
-                     <img src="./images/STATISTICS_IMG.jpg" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awrx_MRWxmRm1R4hd169HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzc1NDI0MDQzNGU0MjU1NmE4NDgwNTg5OTA3NzI3ODA2BGdwb3MDMzEEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dstatic%2Bcourse%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D31&w=291&h=151&imgurl=abhipedia.abhimanu.com%2FUP%2FCRS%2FCRS-762-7-12-2021-11-57-25-PM-Static-GK-Course.png&rurl=https%3A%2F%2Fabhipedia.abhimanu.com%2FONC%2F762%2FStatic-GK-Course&size=21.7KB&p=static+course+images&oid=754240434e42556a8480589907727806&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=Courses+-+Static+GK+Course&b=0&ni=160&no=31&ts=&tab=organic&sigr=vSkuOC1AnniI&sigb=CvepP7zwN5qz&sigi=vXED05BqQERa&sigt=A0Dq9VzlyYyF&.crumb=Rex9syo7NUw&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Statistics for Data Science and Business Analysis</h5>
                          <p className="card-text">Understand the fundamentals of statistics. Learn how to work with different types of data. How to plot different types of data.</p>
@@ -78,7 +78,7 @@ export default function All(){
              {/* Card for Leadership Course */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100">
-                     <img src="./images/leadership.jpg" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awrx_MSVxmRmYC0cnBu9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzQzMzcyNWNhNDY1YzZjYzhmZTdiNmFiMjkxYTNjNjMyBGdwb3MDMTAEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dleadership%2Bcourse%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D10&w=1200&h=800&imgurl=biztrainers.co.nz%2Fimages%2FLeadership_Training_Course.jpg&rurl=https%3A%2F%2Fbiztrainers.co.nz%2Fcourses%2Fleadership-management&size=234.9KB&p=leadership+course+images&oid=433725ca465c6cc8fe7b6ab291a3c632&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=Leadership+%26+Management&b=0&ni=160&no=10&ts=&tab=organic&sigr=e.zIcjNrPGyg&sigb=QqeGzxv_kRRm&sigi=gf5hsa5G5H45&sigt=KvHtmFFV59GT&.crumb=Rex9syo7NUw&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Leadership: Essential for Career Development</h5>
                          <p className="card-text">You will learn the skill sets necessary to lead at all levels and excel in your career.</p>
@@ -90,7 +90,7 @@ export default function All(){
              {/* Card for Anger Management Course */}
              <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3 py-3">
                  <div className="card h-100">
-                     <img src="./images/AngerManagement-img.png" className="card-img-top" alt="" />
+                     <img src="https://in.images.search.yahoo.com/images/view;_ylt=Awrx_MTOxmRmhfYhgRe9HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzhmZjc1YzU0NTZlZDFhOTdmZTZhYTQ1YTY4NWRkYjRkBGdwb3MDMTcEaXQDYmluZw--?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Danger%2Bmanagement%2Bcourse%2Bimages%26ei%3DUTF-8%26type%3DE210IN826G0%26fr%3Dmcafee%26fr2%3Dp%253As%252Cv%253Ai%252Cm%253Asb-top%26tab%3Dorganic%26ri%3D17&w=1920&h=1080&imgurl=s3.amazonaws.com%2Fcoursesity-blog%2F2021%2F04%2FAnger-Management_Classes.png&rurl=https%3A%2F%2Fblog.coursesity.com%2Fbest-anger-management-classes%2F&size=120.3KB&p=anger+management+course+images&oid=8ff75c5456ed1a97fe6aa45a685ddb4d&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&fr=mcafee&tt=6%2B+Best+Anger+Management+Classes+in+2021&b=0&ni=160&no=17&ts=&tab=organic&sigr=UoEVJAR4UqJD&sigb=d6B7mq57lqYp&sigi=YG595qzEHkQT&sigt=jNow7x0hcfK5&.crumb=Rex9syo7NUw&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&type=E210IN826G0" className="card-img-top" alt="" />
                      <div className="card-body">
                          <h5 className="card-title">Anger Management</h5>
                          <p className="card-text">UNLEASHING the Power of Emotions: Mastering Anger Dynamics. DECODE Your Anger: Unlocking the Secrets of Emotional Recognition. STAY Cool Under Pressure: Instant Anger Management Techniques.</p>
